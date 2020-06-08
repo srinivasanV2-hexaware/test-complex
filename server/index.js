1a383452e0c4d9d86f0dc70164a6be2a57536529
srinivasan V <srini@techmindsllc.com>
2020-06-08 14:43:59 +0530

    new data commit

---
+++ server/index.js
@@ -0,0 +1,91 @@
+const keys = require("./keys");
+
+// Express App Setup
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Postgres Client Setup
+const { Pool } = require("pg");
+const pgClient = new Pool({
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort
+});
+
+pgClient.on("error", () => console.log("Lost PG connection"));
+
+pgClient
+  .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+  .catch(err => console.log(err));
+
+// Redis Client Setup
+const redis = require("redis");
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+const redisPublisher = redisClient.duplicate();
+
+// Express route handlers
+
+app.get("/", (req, res) => {
+  res.send("Hi");
+});
+
+app.get("/values/all", async (req, res) => {
+  console.log("server->index.js -> Called /values/all method");
+
+  const values = await pgClient.query("SELECT * from values");
+  console.log("values --", values.rows);
+  res.send(values.rows);
+});
+
+app.get("/values/current", async (req, res) => {
+  console.log("server->index.js -> Called /values/current method");
+
+  redisClient.hgetall("values", (err, values) => {
+    console.log("server->index.js -> Redis values" + values);
+    res.send(values);
+  });
+});
+
+app.post("/values", async (req, res) => {
+  const index = req.body.index;
+  console.log("server->index.js -> Called /values method");
+
+  if (parseInt(index) > 40) {
+    return res.status(422).send("Index too high");
+  }
+
+  redisClient.hset("values", index, "Nothing yet!");
+  redisPublisher.publish("insert", index);
+
+  try {
+    pgClient.query(
+      "INSERT INTO values(number) VALUES($1)",
+      [index],
+      (err, result) => console.log(err)
+    );
+    console.log("inserting postgres value..");
+  } catch {
+    console.log(err);
+  }
+
+  //pgClient.query("INSERT INTO values(number) VALUES($1)", [index]);
+
+  console.log("server->index.js -> Entry into DB done /values method");
+
+  res.send({ working: true });
+});
+
+app.listen(5000, err => {
+  console.log("Listening");
+});
